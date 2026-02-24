b5e2e8290c047a08f6341ae8fe0c0090d88145f1
student <sakir.demiral@sbs-herzogenaurach.de>
2026-02-24 08:43:34 +0100

    "Zurück" Button hinzugefügt

--- src/MainWindow.py
+++ src/MainWindow.py
@@ -1,7 +1,15 @@
+# ------------------------------------------------------------------------------
+# Projekt: BooktrackQR
+# Modul: MainWindow (GUI)
+# Autoren: Mustafa Demiral, Ahmet Toplar
+# Stand: GUI mit Logik
+# ------------------------------------------------------------------------------
+
+
 from PyQt6.QtWidgets import QMainWindow, QStackedWidget
 
 from CentralWidget import CentralWidget
-from Schuelerverwaltung import SchuelerverwaltungWidget # <-- Hier ist der Name angepasst!
+from Schuelerverwaltung import SchuelerverwaltungWidget
 
 class MainWindow(QMainWindow):
     def __init__(self, parent=None):
@@ -10,7 +18,7 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("BooktrackQR")
         self.setMinimumSize(900, 700)
         self.setStyleSheet("QWidget { background-color: white; }")
-
+#Ahmet
         # 1. Den "Kartenstapel" erstellen
         self.stacked_widget = QStackedWidget(self)
         self.setCentralWidget(self.stacked_widget)
@@ -18,14 +26,17 @@ class MainWindow(QMainWindow):
         # 2. Deine erstellten Bildschirme laden
         self.main_menu_widget = CentralWidget()
         self.schueler_widget = SchuelerverwaltungWidget()
-
+#Mustafa
         # 3. Bildschirme zum Stapel hinzufügen
         self.stacked_widget.addWidget(self.main_menu_widget)
         self.stacked_widget.addWidget(self.schueler_widget)
 
         # 4. Den Button aus dem Hauptmenü mit der Wechsel-Funktion verbinden
         self.main_menu_widget.btn_schueler.clicked.connect(self.zeige_schuelerverwaltung)
-
+#Ahmet
+        # Schülerverwaltung -> Hauptmenü (NEU)
+        self.schueler_widget.btn_back.clicked.connect(self.zeige_hauptmenue)  # <-- NEU
+#Mustafa
     def zeige_schuelerverwaltung(self):
         """Wechselt zur Schülerverwaltung (Index 1)"""
         self.stacked_widget.setCurrentIndex(1)
