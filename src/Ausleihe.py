faaf80d9920f617c8be818fadc2a7fd33408dd97
danielpopp <Daniel.Popp@sbs-herzogenaurach.de>
2026-03-11 10:03:58 +0100

    PBI Ausleihe - Modul (zweite Version) mit Anpassung -Daniel Popp & Batuhan Aktürk

--- src/Ausleihe.py
+++ src/Ausleihe.py
@@ -431,7 +431,7 @@ class AusleiheWidget(QWidget):
 
         bottom_actions.addStretch()
 
-        self.btn_next_student = QPushButton("✖ Nächster Schüler (Reset)")
+        self.btn_next_student = QPushButton("Zurückstzen")
         self.btn_next_student.setStyleSheet("""
             QPushButton { background-color: #E0E0E0; color: #333333; padding: 12px 25px; border: 3px solid #E0E0E0; border-radius: 6px; font-weight: bold; font-size: 13px; }
             QPushButton:hover { border: 3px solid #333333; }
@@ -440,7 +440,7 @@ class AusleiheWidget(QWidget):
         self.btn_next_student.setEnabled(False)
         bottom_actions.addWidget(self.btn_next_student)
 
-        self.btn_finish = QPushButton("✔ Ausleihe abschließen")
+        self.btn_finish = QPushButton("Ausleihe abschließen")
         self.btn_finish.setStyleSheet(f"""
             QPushButton {{ background-color: {self.COLOR}; color: white; padding: 12px 25px; border: 3px solid {self.COLOR}; border-radius: 6px; font-weight: bold; font-size: 13px; }}
             QPushButton:hover {{ border: 3px solid #333333; }}
